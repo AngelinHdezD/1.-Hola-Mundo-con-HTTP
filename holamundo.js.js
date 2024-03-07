e6d2cf72e72ec9fb01c15d6b8dfa556a384e26a0
AngelinHdezD <tingzebra@gmail.com>
2024-03-07 09:41:10 -0600

    2do Commit

--- holamundo.js.js
+++ holamundo.js.js
@@ -3,8 +3,8 @@ const hostname = '192.168.1.140';
 const port = 3000;
 const server = http.createServer((req, res) => {
     res.statsCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('<h1>Hola Mundo, Angel Hernandez Diaz<h1>');
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Hola Mundo, Angel Hernandez Diaz</h1>');
 });
 
 server.listen(port,hostname, ()=>{
