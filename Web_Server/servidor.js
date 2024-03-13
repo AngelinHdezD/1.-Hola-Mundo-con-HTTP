d9b4a23a13926dcffb7f8c6524344e43e2288b0e
AngelinHdezD <tingzebra@gmail.com>
2024-03-13 09:17:44 -0600

    UBUNTU SERVER

--- Web_Server/servidor.js
+++ Web_Server/servidor.js
@@ -31,7 +31,7 @@ contentType = mimeTypes[extname] || 'application/octet-stream';
 fs.readFile(filePath, function(error, content) {
 if (error){
     if(error.code == 'ENOENT'){
-        fs.readFile('./404.html', function(error, content){
+        fs.readFile('./4.html', function(error, content){
             response.writeHead(200, {'Content-Type': contentType});
             response.end(content, 'utf-8');
         });
@@ -48,4 +48,4 @@ if (error){
     }
 });
 }).listen(3000);
-console.log('Server running at http://127.0.0.1:3000/');
+console.log('Server running at http://angelhdezubuntu.fullstack.com.mx:3000/');
